b9e43866b32116a606faf7e4b42ff1a65d3a02be
Olga Malkova <Vodeviiil@mail.ru>
2023-04-11 16:52:51 +0300

    практическое задание 8.10

--- src/main/java/practice/Birthdays.java
+++ src/main/java/practice/Birthdays.java
@@ -1,23 +1,29 @@
 package practice;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 public class Birthdays {
-
     public static void main(String[] args) {
-
-        int day = 31;
-        int month = 12;
-        int year = 1990;
-
+        int day = 15;
+        int month = 5;
+        int year = 1992;
         System.out.println(collectBirthdays(year, month, day));
-
     }
-
     public static String collectBirthdays(int year, int month, int day) {
 
-        //TODO реализуйте метод для построения строки в следующем виде
-        //0 - 31.12.1990 - Mon
-        //1 - 31.12.1991 - Tue
-        
-        return "";
+        LocalDate birthday = LocalDate.of(year, month, day);
+        LocalDate today = LocalDate.now();
+        int count = 0;
+        String text = "";
+        DateTimeFormatter formatter1 = DateTimeFormatter.ofPattern("dd.MM.yyyy - E", Locale.ENGLISH);
+
+        while ((birthday.isBefore(today))||(birthday.isEqual(today))){
+            text = text + count + " - " + formatter1.format(birthday) + "\n";
+            birthday = birthday.plusYears(1);
+            count++;
+
+        }
+        return text;
     }
 }
--- src/main/java/practice/TrucksAndContainers.java
+++ src/main/java/practice/TrucksAndContainers.java
@@ -1,28 +1,46 @@
 package practice;
-
 import java.util.Scanner;
-
 public class TrucksAndContainers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int boxes = scanner.nextInt();  //получение количество коробок от пользователя
+        int containers = 1;
+        int trucks = 1;
 
-        //получение количество коробок от пользователя
-        int boxes = scanner.nextInt();
+        if (boxes == 0){
+            containers = 0;
+            trucks = 0;
+            System.out.println("Необходимо:");
 
-        // TODO: вывести в консоль коробки разложенные по грузовикам и контейнерам
-        // пример вывода при вводе 2
-        // для отступа используйте табуляцию - \t
+            System.out.println("грузовиков - " + trucks + " шт.");
+            System.out.println("контейнеров - " + containers + " шт.");
+        }
+        if (boxes != 0){
+            System.out.println("Грузовик: " + trucks);
+            System.out.println("\tКонтейнер: " + containers);
 
-        /*
-        Грузовик: 1
-            Контейнер: 1
-                Ящик: 1
-                Ящик: 2
-        Необходимо:
-        грузовиков - 1 шт.
-        контейнеров - 1 шт.
-        */
+            for(int i = 1; i <= boxes; i++) {
+                System.out.println("\t\tЯщик: " + i);
+                if (i % 324 == 0) {
+                    trucks++;
+                    System.out.println("Грузовик: " + trucks);
+                }
+                if (i % 27 == 0) {
+                    if (boxes == 27 ){
+                        break;
+                    }
+                    if (boxes != 27 ) {
+                        containers++;
+                        System.out.println("\tКонтейнер: " + containers);
+                    }
+                }
+            }
+
+            System.out.println("Необходимо:");
+
+            System.out.println("грузовиков - " + trucks + " шт.");
+            System.out.println("контейнеров - " + containers + " шт.");
+        }
     }
-
 }
